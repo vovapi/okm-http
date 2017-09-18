ae0553972d4f10cea7025c47e5e7dbdaa343fdd0
Vladimir Evgrafov <evgrafov.vladimir@gmail.com>
2017-09-18 19:48:42 +0300

    gofmt

--- http.go
+++ http.go
@@ -1,18 +1,18 @@
 package okm_http
 
 import (
-	"github.com/okmeter/tcpkeepalive"
+	"context"
+	"errors"
 	"github.com/okmeter/dns"
+	"github.com/okmeter/tcpkeepalive"
+	"io"
 	"math/rand"
 	"net"
 	"net/http"
-	"runtime"
-	"time"
-	"io"
 	"net/url"
+	"runtime"
 	"strings"
-	"errors"
-	"context"
+	"time"
 )
 
 type Client struct {
@@ -46,18 +46,18 @@ func (c *Client) resolve(host string) ([]string, error) {
 	addrs, err := resolver.LookupHost(ctx, host)
 	if len(addrs) == 0 || err != nil {
 		//	Fallback on 8.8.8.8
-    	client := dns.Client{DialTimeout:c.ResolveTimeout}
-    	msg := dns.Msg{}
-    	msg.SetQuestion(host+".", dns.TypeA)
-    	r, _, err := client.Exchange(&msg, defaultNs + ":53")
-    	if err != nil {
-    		return nil, err
+		client := dns.Client{DialTimeout: c.ResolveTimeout}
+		msg := dns.Msg{}
+		msg.SetQuestion(host+".", dns.TypeA)
+		r, _, err := client.Exchange(&msg, defaultNs+":53")
+		if err != nil {
+			return nil, err
 		}
 		addrs := []string{}
-    	for _, ans := range r.Answer {
-    		addr := ans.(*dns.A)
-    		addrs = append(addrs, addr.A.String())
-    	}
+		for _, ans := range r.Answer {
+			addr := ans.(*dns.A)
+			addrs = append(addrs, addr.A.String())
+		}
 	}
 	return addrs, nil
 }
@@ -108,31 +108,29 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 // From net/http
 
 func (c *Client) Get(url string) (resp *http.Response, err error) {
-  	req, err := http.NewRequest("GET", url, nil)
-  	if err != nil {
-  		return nil, err
-  	}
-  	return c.Do(req)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return c.Do(req)
 }
 
-
 func (c *Client) Head(url string) (resp *http.Response, err error) {
-  	req, err := http.NewRequest("HEAD", url, nil)
-  	if err != nil {
-  		return nil, err
-  	}
-  	return c.Do(req)
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return c.Do(req)
 }
 
 func (c *Client) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
-  	req, err := http.NewRequest("POST", url, body)
-  	if err != nil {
-  		return nil, err
-  	}
-  	req.Header.Set("Content-Type", contentType)
-  	return c.Do(req)
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", contentType)
+	return c.Do(req)
 }
-
 
 func (c *Client) PostForm(url string, data url.Values) (resp *http.Response, err error) {
 	return c.Post(url, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
